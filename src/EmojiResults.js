2150787cf53fce646a1e260708f55a8aa4d00ec5
nileshgunjkar143 <33544329+nileshgunjkar143@users.noreply.github.com>
2021-12-23 15:30:49 +0530

    Update EmojiResults.js

--- src/EmojiResults.js
+++ src/EmojiResults.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import Clipboard from "clipboard";
 
 import EmojiResultRow from "./EmojiResultRow";
 
@@ -8,14 +7,6 @@ export default class EmojiResults extends PureComponent {
   static propTypes = {
     emojiData: PropTypes.array
   };
-
-  componentDidMount() {
-    this.clipboard = new Clipboard(".copy-to-clipboard");
-  }
-
-  componentWillUnmount() {
-    this.clipboard.destroy();
-  }
 
   render() {
     return (
